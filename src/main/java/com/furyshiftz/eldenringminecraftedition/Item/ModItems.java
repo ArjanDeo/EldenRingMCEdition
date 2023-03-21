9d7e701fe7c90c769ec947d0528fa33958c1c973
ArjanDeo <arjandeo@bluecomet-it.com>
2023-03-21 20:39:19 +0000

    Added Item "Spirit Calling Bell [EPIC]".

--- src/main/java/com/furyshiftz/eldenringminecraftedition/Item/ModItems.java
+++ src/main/java/com/furyshiftz/eldenringminecraftedition/Item/ModItems.java
@@ -13,9 +13,10 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, EldenRingMC.MOD_ID);
+    public static final RegistryObject<Item>  SPIRIT_CALLING_BELL = ITEMS.register("spirit_calling_bell", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.ELDEN_RING_TAB).stacksTo(1)));
+
     public static final RegistryObject<Item>  MYTHRITE_INGOT = ITEMS.register("mythrite_ingot", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.ELDEN_RING_TAB)));
     public static final RegistryObject<Item>  GOSKIN_APOSTLE_DISC = ITEMS.register("godskin_apostle_music_disc", () -> new RecordItem(4, ModSounds.GODSKIN_APOSTLE_THEME, new Item.Properties().tab(ModCreativeModeTab.ELDEN_RING_TAB).stacksTo(1)));
-
     public static final RegistryObject<Item>  SCEPTER_ALL_KNOWING = ITEMS.register("scepter_of_the_all_knowing", () -> new ScepterAllKnowingItem(new Item.Properties().tab(ModCreativeModeTab.ELDEN_RING_TAB).stacksTo(1)));
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
